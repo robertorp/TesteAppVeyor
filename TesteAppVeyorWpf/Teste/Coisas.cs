f5286efef9bb6fdf93ff7dbd4b59fdb13d18c15e
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-01-22 14:06:46 -0200

    Adicionado outro tipo de projeto de teste

---
+++ TesteAppVeyorWpf/Teste/Coisas.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Teste
+{
+    [TestClass]
+    public class Coisas
+    {
+        [TestMethod]
+        public void Soma()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void Pokemon()
+        {
+            Assert.AreEqual("Charmander", "Charmander");
+        }
+    }
+}
