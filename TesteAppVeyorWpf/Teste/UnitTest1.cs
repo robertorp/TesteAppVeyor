9a66823e9c94b74115469c281d112b33bcc32357
Roberto Alves Pereira <robertoalvespereira18@gmail.com>
2017-01-22 13:59:13 -0200

    Testes

---
+++ TesteAppVeyorWpf/Teste/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Teste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Soma()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
